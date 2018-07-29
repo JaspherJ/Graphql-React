aa5ba5836e531a3368d5e734f3c3404926f18f19
JaspherJ <jazoswald@gmail.com>
2018-07-29 19:46:21 -0400

    added new queries and mutations

---
+++ Lyrical-GraphQL-master/client/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList'
+const client = new ApolloClient({});
+
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+  <SongList/>
+  </ApolloProvider>
+);
+};
+
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+);
