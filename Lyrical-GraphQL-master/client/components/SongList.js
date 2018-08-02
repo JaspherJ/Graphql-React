22b86e5ee341a3350a6d939c16f06bf42dc74210
JaspherJ <jazoswald@gmail.com>
2018-08-01 23:10:49 -0400

    new conponents added

--- Lyrical-GraphQL-master/client/components/SongList.js
+++ Lyrical-GraphQL-master/client/components/SongList.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
 
 class SongList extends Component {
-  render(){
-    return(
-        <div>
-        SongList
-   </div>
-    )
+    renderSongs(){
+        
+        return this.props.data.songs.map(song => {
+            return (
+                <li key={song.id} className="collection-item">
+                {song.title}
+                </li>
+            );
+        });
     
+    }
+
+
+  render(){
+    if(this.props.data.loading) {
+        return <div>Loading...</div>
+    }
+    console.log(this.props);
+    return(
+        <ul className="collection">
+        {this.renderSongs()}
+   </ul>
+    )
  }
-
-
 }
 
-export default SongList;
+const query =gql`
+{
+    songs{
+        id
+        title
+    }
+  }
+  
+`;
+
+export default graphql(query)(SongList);
