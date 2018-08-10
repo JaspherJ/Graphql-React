583a7ecc093b50abd455349d6e66f327651e4740
JaspherJ <jazoswald@gmail.com>
2018-08-09 23:51:59 -0400

    added lyric and like components

--- Lyrical-GraphQL-master/client/index.js
+++ Lyrical-GraphQL-master/client/index.js
@@ -6,11 +6,13 @@ import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 
 import App from "./components/App";
-
+import SongDetail from "./components/SongDetail";
 import SongList from "./components/SongList";
 import SongCreate from "./components/SongCreate";
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
@@ -19,6 +21,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
           <route path="songs/new" component={SongCreate} />
+          <route path="songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
